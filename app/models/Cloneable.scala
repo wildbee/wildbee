82083c4fcb821041348a7f1221eef5e39490adda
Francisco Canas <fcanas@redhat.com>
2013-11-29 09:29:19 -0500

    Adding a Cloneable trait for entity models that are supposed to have the clone functionality.

---
+++ app/models/Cloneable.scala
@@ -0,0 +1,28 @@
+package models
+
+import play.api.Play.current
+import play.api.db.slick.Config.driver.simple._
+import play.api.db.slick.DB
+import scala.slick.session.Session
+import java.util.UUID
+import scala.language.reflectiveCalls
+
+/**
+ * This trait is to allow models to create clones of themselves in the database.
+ * Requires a Queriable object and all of its restrictions.
+ * @tparam T
+ */
+trait Cloneable[T <: AnyRef with Queriable[T] { val id: UUID; val name: String }] {
+  self: Table[T] =>
+
+  def id: Column[UUID]
+  def name: Column[String]
+  def find: T
+  def newId: UUID
+
+//  def clone(oid: UUID): UUID = DB.withSession {
+//    implicit session: Session =>
+//
+//  }
+
+ }
